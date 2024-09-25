66be8a654b7ec1bf22cc01e2c021184321c30efb
Akhil Reddy <Abijjam3>
2024-09-24 21:04:17 -0500

    Sample

---
+++ src/Sample.java
@@ -0,0 +1,9 @@
+public class Sample {
+    public static void main(String[] args) {
+        String name = "Akhil";
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i + ": " + name);
+        }
+    }
+}
